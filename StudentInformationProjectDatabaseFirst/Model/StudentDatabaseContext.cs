196f7ef574f6a24c89fc1d3ebf3054a0bde2b194
andromegasoftware <kadiryapar16@gmail.com>
2023-02-03 09:19:58 +0200

    first commit

---
+++ StudentInformationProjectDatabaseFirst/Model/StudentDatabaseContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentInformationProjectDatabaseFirst.Model;
+
+public partial class StudentDatabaseContext : DbContext
+{
+    public StudentDatabaseContext()
+    {
+    }
+
+    public StudentDatabaseContext(DbContextOptions<StudentDatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<StudentDetail> StudentDetails { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<StudentDetail>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.City)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.Country)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.Surname)
+                .HasMaxLength(50)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ StudentInformationProjectDatabaseFirst/Model/StudentDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentInformationProjectDatabaseFirst.Model;
+
+public partial class StudentDetail
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Surname { get; set; }
+
+    public string? City { get; set; }
+
+    public string? Country { get; set; }
+}
